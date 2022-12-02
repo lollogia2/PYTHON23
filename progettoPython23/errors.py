3876d96f2a5f7d79e5a2041b0e514c43d2fbdf73
Lorenzo Giacometti <lorenzo.giacometti@edu.unito.it>
2022-12-03 00:43:48 +0100

    spezzettato in vari file

---
+++ progettoPython23/errors.py
@@ -0,0 +1,9 @@
+from tkinter import messagebox
+
+
+def fields():
+    messagebox.showerror('ERRORE', 'Campi vuoti o caratteri errati!')
+
+
+def fields2():
+    messagebox.showerror('ERRORE', 'parola del cazzo!')
