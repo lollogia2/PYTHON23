edf060e1ec3c16c4af6cdfa1c1120427ba0ee3f5
Lorenzo Giacometti <lorenzo.giacometti@edu.unito.it>
2023-02-09 01:03:20 +0100

    fix vari

--- progettoPython23/find_word.py
+++ progettoPython23/find_word.py
@@ -35,46 +35,56 @@ def get_dictionary_word_list():
     return data_into_list
 
 
+# ritorna tutte le abbreviazioni (di una lettera) di una data 'word'
 def shortened_words(word):
-    global loop_counter
-    loop_counter = loop_counter + 1
-    if loop_counter < 65000:
-        print(loop_counter)
-        for i in range(len(word)):
-            yield word[:i] + word[i + 1:], i
-    else:
-        typeError: SystemError
-        return None
+    # print(loop_counter)
+    for i in range(len(word)):
+        yield word[:i] + word[i + 1:], i
 
 
+# crea un dizionario che ha come chiave la paerola abbreviata e come elemento la parola intera
 def make_graph(d):
     g = defaultdict(list)
     for word in d:
         for short in shortened_words(word):
             g[short].append(word)
+    print(g)
     return g
 
 
+# to-do = lista di parole utilizzabili seguendo le 3 regole
+# seen  = dizionario contenente tutte le parole comprese le sue radici
 def walk_graph(g, d, start, end):
+    global loop_counter
     todo = deque([start])
     seen = {start: None}
     while todo:
-        word = todo.popleft()
-        if word == end:
-            break
+        # print(to-do)
+        if loop_counter < 600:
+            # print(loop_counter)
+            word = todo.popleft()
+            if word == end:
+                break
 
-        same_length = chain(*(g[short] for short in shortened_words(word)))  # * print the list separated by spaces
-        one_longer = chain(*(g[word, i] for i in range(len(word) + 1)))
-        one_shorter = (w for w, i in shortened_words(word) if w in d)
-        anagram = make_anagrams(word, d)
-        for next_word in chain(same_length, one_longer, one_shorter, anagram):
-            if next_word not in seen:
-                seen[next_word] = word
-                todo.append(next_word)
+            same_length = chain(*(g[short] for short in shortened_words(word)))  # * print the list separated by spaces
+            one_longer = chain(*(g[word, i] for i in range(len(word) + 1)))
+            one_shorter = (w for w, i in shortened_words(word) if w in d)
+            anagram = make_anagrams(word, d)
+            for next_word in chain(same_length, one_longer, one_shorter, anagram):
+                if next_word not in seen:
+                    seen[next_word] = word
+                    # print(seen)
+                    todo.append(next_word)
+            loop_counter = loop_counter + 1
+        else:
+            typeError: SystemError
+            return None
+
     else:  # no break, i.e. not reachable
         return None  # not reachable
 
     path = [end]
+    # print(seen)
     while path[-1] != start:
         path.append(seen[path[-1]])
     return path[::-1]
